2c5ce75c73e065e1bdfb4fe8af6c14a1a4713086
Borntokilltheworld <77736929+Borntokilltheworld@users.noreply.github.com>
2021-08-25 13:37:27 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -3,5 +3,5 @@ function adduser(){
     player2_name = document.getElementById("player2_name_input").value;
     localStorage.setItem("player1name",player1_name);
     localStorage.setItem("player2name",player2_name);
-    window.location = "game_page.html";
+    window.location = "quiz_game_page.html";
 }
--- quiz_game_page.js
+++ quiz_game_page.js
@@ -6,11 +6,11 @@ document.getElementById("player1_name").innerHTML = player1_name+" : ";
 document.getElementById("player2_name").innerHTML = player2_name+" : ";
 document.getElementById("player1_score").innerHTML = player1_score;
 document.getElementById("player2_score").innerHTML = player2_score;
-document.getElementById("player_question").innerHTML = "question turn : "+player1_name;
+document.getElementById("player_question").innerHTML = "Question turn : "+player1_name;
 document.getElementById("player_answer").innerHTML = "Answer Turn : "+player2_name;
 function send(){
-    number1 = document.getElementById("number1").value = number1;
-    number2 = document.getElementById("number2").value = number2;
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
     actual_answer = parseInt(number1)*parseInt(number2)
     console.log("actual_answer");
     questionnumber = "<h4>"+number1+"X"+number2+"</h4>";
@@ -21,3 +21,35 @@ function send(){
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
 }
+question_turn = "player1";
+answer_turn = "player2";
+function check(){
+    get_answer = document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer){
+        if(answer_turn == "player1"){
+            player1_score = player1_score+1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else{
+            player2_score = player2_score+1;
+            document.getElementById("player2_score").innerHTML = player2_score;
+        }
+    }
+    if(question_turn=="player1"){
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Question turn : "+player2_name;
+    }
+    else{
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Question turn : "+player1_name;
+    }
+    if(answer_turn == "player1"){
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer turn : "+player2_name;
+    }
+    else{
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer turn : "+player1_name;
+    }
+    document.getElementById("output").innerHTML = "";
+    }
